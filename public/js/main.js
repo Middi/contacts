855af8011dd5529684e9291ac4d2bef2ecaabad2
“Richard Middleton” <Richard@richardmiddleton.me>
2017-07-16 19:14:45 +0200

    stripped table

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 $( document ).ready(function(){
 
     $(".button-collapse").sideNav();
-
+    // $('.collection-item:visible:odd').addClass('odd');
+    // $('.collection-item:visible:even').css({'background-color': 'yellow'});
 });
