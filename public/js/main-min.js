27a20be1073677b318372d70b232df88b4212e7b
“Richard Middleton” <Richard@richardmiddleton.me>
2017-07-20 17:16:06 +0200

    list-item has own div with options onclick

--- public/js/main-min.js
+++ public/js/main-min.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  
   $('#search').click(function () {
     $("#search-bar").toggleClass("pad");
     $("#search-input").slideToggle("slow", function () {
@@ -9,6 +10,27 @@ $(document).ready(function () {
     $("#add-bar").slideToggle("slow", function () {
     });
   });
+
+  // $('.list-item').click(function () {
+  //   $(this).children('.action').toggle();
+  //   $(this).height(140);
+  // });
+
+
+  $('.list-item').click(function () {
+    if ($(this).children('.action').hasClass('hide')) {
+      $(this).animate({ height: 140 }, 200, function () {
+
+        $(this).children('.action').fadeIn().removeClass('hide');
+
+      });
+    } else {
+      $(this).animate({ height: 80 }, 200, function () {
+        $(this).children('.action').fadeOut().addClass('hide');
+      });
+    }
+  });
+
 
   // Search function
 
